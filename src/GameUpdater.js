f65cecc301c803625a385e728a600ec2191759f5
zephyz <zephyz.zhaar@gmail.com>
2014-10-11 14:23:20 +0200

    implemented gameupdated

--- src/GameUpdater.js
+++ src/GameUpdater.js
@@ -1,14 +1,34 @@
+var _ = require('lodash');
+
+var helpers = require('./helpers');
+var stringify = helpers.stringify;
+
+var Pool = require('./Pool');
+var Connection = require('./Connection')
+
 var actions = {
     'get:gamestate': getCurrentGamestate,
     'set:commande': setCommande
 };
 
-function getCurrentGamestate(conn, data) {
+function gameStateByUser(user) {
+    return Pool.getByLabel('games')
+        .getByUser(user)
+        .gameState;
+}
 
+function getCurrentGamestate(conn, data) {
+    Connection.getByUser(conn.user).socket.write(stringify({
+        status: "success",
+        gameState: gameStateByUser(conn.user)
+    }));
 }
 
 function setCommande(conn, data) {
-
+    var game = Pool.getByLabel('games').getByUser(conn.user);
+    var user = _.where(game.players, {id: conn.user.id});
+    debugger;
+    user.command = data.value.setCommande;
 }
 
 module.exports = {
