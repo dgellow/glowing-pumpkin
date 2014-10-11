9ba712efc38b9f5f0e4a78e198d8029bf431c8ed
dgellow <samuel.elborai@gmail.com>
2014-10-11 12:08:04 +0200

    generate player stats

--- src/User.js
+++ src/User.js
@@ -7,9 +7,25 @@ var dbUsers = new db('users');
 var _users = require('../data/data_test.json').Users;
 var allUsers;
 
+function dice(faces) {
+    return Math.ceil(Math.random() * faces);
+}
+
 var User = function(id, name) {
     this.id = id;
     this.name = name;
+
+    this.level = 1;
+
+    this.currencies = {
+        candies: 110
+    };
+
+    // player stats
+    this.attack = 3 * dice(6);
+    this.speed = 3 * dice(6);
+    this.defense = 3 * dice(6);
+    this.hp = 3 + dice(6);
 };
 
 User.create = function(id, name) {
