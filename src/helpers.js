aec7532aef1dfd23d0495137b029d1cb9e5613f5
zephyz <zephyz.zhaar@gmail.com>
2014-10-12 15:10:27 +0200

    fix typo

--- src/helpers.js
+++ src/helpers.js
@@ -29,7 +29,7 @@ function stringify(obj) {
         strData = JSON.stringify(obj);
     } catch(err) {
         log('!! Exception: ' + util.inspect(err));
-        strData = JSON.stringify({status: 'error', message: 'server exception '});
+        strData = JSON.stringify({ status: 'error', message: 'server exception' });
     } finally {
         return strData;
     }
